ffdfda290c62d2b1b821e7514eb8f519c17daf7d
Zhipeng Zhao <zzhao1@andrew.cmu.edu>
2017-09-22 11:55:31 -0400

    separate wrapper

--- config.mk
+++ config.mk
@@ -111,7 +111,7 @@ THREAD_OBJS = malloc.o panic.o
 ###########################################################################
 # Object files for your syscall wrappers
 ###########################################################################
-SYSCALL_OBJS = syscall.o syscall_wrapper.o
+SYSCALL_OBJS = syscall.o set_status.o vanish.o
 
 ###########################################################################
 # Object files for your automatic stack handling
