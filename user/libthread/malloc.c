25ab25ba5df76009d0051f8fd184ec643d97f423
cheyuanl <cheyuanl@andrew.cmu.edu>
2017-09-26 22:45:06 -0400

    Delegate work to malloc lib

--- user/libthread/malloc.c
+++ user/libthread/malloc.c
@@ -11,20 +11,21 @@
 
 void *malloc(size_t __size)
 {
-  return NULL;
+  return _malloc(__size);
 }
 
 void *calloc(size_t __nelt, size_t __eltsize)
 {
-  return NULL;
+  return _calloc(__nelt, __eltsize);
 }
 
 void *realloc(void *__buf, size_t __new_size)
 {
-  return NULL;
+  return _realloc(__buf, __new_size);
 }
 
 void free(void *__buf)
 {
+  _free(__buf);
   return;
 }
