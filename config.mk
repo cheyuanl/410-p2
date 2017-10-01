5e571b4dff61e2fac32f72ceada6d6f62afb6343
Zhipeng Zhao <zzhao1@andrew.cmu.edu>
2017-09-30 23:25:41 -0400

    passed the misbehave startle

--- config.mk
+++ config.mk
@@ -93,7 +93,8 @@ UPDATE_METHOD = afs
 # A list of the test programs you want compiled in from the user/progs
 # directory
 #
-STUDENTTESTS = test_malloc test_thr_create our_actual_wait
+STUDENTTESTS = test_malloc test_thr_create our_actual_wait\
+               our_misbehave
 
 ###########################################################################
 # Object files for your thread library
